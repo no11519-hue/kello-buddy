f596f26cb785aa9750b408cf6b82b1a90a44af69
parkkyunhgo <polo7402@gmail.com>
2026-03-19 16:51:05 +0900

    Fix mobile image truncation in GlobalTrendSection

--- src/components/GlobalTrendSection.tsx
+++ src/components/GlobalTrendSection.tsx
@@ -22,14 +22,14 @@ const GlobalTrendSection = () => {
         </ScrollReveal>
       </div>
 
-      {/* Full width image container with precise responsive crop */}
-      <div className="w-full relative z-10 overflow-hidden">
+      {/* Relative container with controlled overflow to prevent image truncation */}
+      <div className="w-full relative z-10">
         <ScrollReveal width="100%">
-          <div className="relative w-full overflow-hidden bg-[#f9ebf3] -mt-[18vw] sm:-mt-[15vw] md:-mt-[180px]">
+          <div className="relative w-full overflow-hidden bg-[#f9ebf3] -mt-[14vw] sm:-mt-[12vw] md:-mt-[170px]">
             <img 
               src="/그래프11.png" 
               alt="글로벌 관광객 유입과 K-뷰티 소비 트렌드 그래프" 
-              className="w-full h-auto object-cover object-bottom"
+              className="w-full h-auto object-contain"
               style={{ 
                 imageRendering: 'auto', 
                 WebkitBackfaceVisibility: 'hidden',
